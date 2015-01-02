dd280a852d7917729a60627b9dc96844176410ca
Amir Eibagi <Amir@Amirs-MacBook-Pro.local>
2015-01-02 11:38:09 -0800

    Added test class

---
+++ Git Test/TestFile.h
@@ -0,0 +1,13 @@
+//
+//  TestFile.h
+//  Git Test
+//
+//  Created by Amir Eibagi on 1/2/15.
+//  Copyright (c) 2015 Amir Eibagi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestFile : NSObject
+
+@end
---
+++ Git Test/TestFile.m
@@ -0,0 +1,13 @@
+//
+//  TestFile.m
+//  Git Test
+//
+//  Created by Amir Eibagi on 1/2/15.
+//  Copyright (c) 2015 Amir Eibagi. All rights reserved.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
